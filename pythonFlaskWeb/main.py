1afa2a6778a563ee7acf3c11b99da29af979e8fe
Максим Кукушкин <maxkuku@gmail.com>
2023-04-12 17:18:42 +0300

    Пройден 10 урок
    Конечно же почта не отправляется из-за неверных настроек smtp

--- pythonFlaskWeb/main.py
+++ pythonFlaskWeb/main.py
@@ -22,7 +22,7 @@ def save_picture(form_picture):
     i = Image.open(form_picture)
     i.thumbnail(output_size)
 
-    form_picture.save(picture_path)
+    i.save(picture_path)
 
     return picture_fn
 
